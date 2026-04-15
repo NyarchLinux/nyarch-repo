631edf83e138de3fc016b60e30e68f0e6e0459ba
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2026-04-15 10:03:36 +0200

    Add repo directory to add nyarch repo

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -1,4 +1,12 @@
 rm nyarch-repo*
 rm *debug*
 
+latest_mirrorlist=$(ls -v nyarch-mirrorlist-*.pkg.tar.zst | tail -1)
+cp "$latest_mirrorlist" repo/nyarch-mirrorlist.pkg.tar.zst
+cp "${latest_mirrorlist}.sig" repo/nyarch-mirrorlist.pkg.tar.zst.sig
+
+latest_keyring=$(ls -v nyarch-keyring-*.pkg.tar.zst | tail -1)
+cp "$latest_keyring" repo/nyarch-keyring.pkg.tar.zst
+cp "${latest_keyring}.sig" repo/nyarch-keyring.pkg.tar.zst.sig
+
 repo-add nyarch-repo.db.tar.gz *.pkg.tar.zst
